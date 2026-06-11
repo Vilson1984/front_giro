6f26615a6d364c66c19a5c08afddec432f4d0e28
Vilson C Monteiro <vcmorion@hotmail.com>
2026-06-10 22:30:14 -0300

    Render at opcoes

--- src/app/features/opcoes-viagem/pages/opcoes-viagem/opcoes-viagem.ts
+++ src/app/features/opcoes-viagem/pages/opcoes-viagem/opcoes-viagem.ts
@@ -1,24 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SolicitacaoViagemService } from '../../../solicitacao-viagem/services/solicitacao-viagem.service';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-opcoes-viagem',
   imports: [CommonModule],
   templateUrl: './opcoes-viagem.html',
-  styleUrl: './opcoes-viagem.scss',
+  styleUrls: ['./opcoes-viagem.scss'],
   standalone: true  
 })
-
 export class OpcoesViagem {
-  dados:any= null;
-  constructor(private solicitacaoViagemService: SolicitacaoViagemService) {
-  this.receberDados();
-}
-receberDados(){
-  this.solicitacaoViagemService.dadosOpcoes.subscribe(dados => {
-    this.dados = dados.optionsDrivers;
-    console.log('Dados recebidos em OpcoesViagem:', dados.optionsDrivers);
-  });
-}
+  dados$!: Observable <any>; 
+  
+  constructor(private solicitacaoViagemService: SolicitacaoViagemService) {}
+  
+  ngOnInit(){
+    this.dados$ = this.solicitacaoViagemService.dadosOpcoes; // Observable direto
+  }
 }
--- src/app/features/solicitacao-viagem/services/solicitacao-viagem.service.ts
+++ src/app/features/solicitacao-viagem/services/solicitacao-viagem.service.ts
@@ -7,22 +7,21 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class SolicitacaoViagemService {
-  constructor(private http: HttpClient) {}
+private enviaDadosOpcoes = new BehaviorSubject<any[]>([]);
+dadosOpcoes = this.enviaDadosOpcoes.asObservable();
 
+  constructor(private http: HttpClient) {}
   estimateRide: any;
 
   calcularViagem(customer_id: string, origin: string, destination: string): Observable<any> {
-    this.estimateRide = this.http.post('http://localhost:3001/ride/estimate', {
+    return this.http.post('http://localhost:3001/ride/estimate', {
       customer_id,
       origin,
       destination,
     });
-    return this.estimateRide;
   }
 
-  private enviaDadosOpcoes = new BehaviorSubject<any>(''); // Inicializa com um valor padrão, se necessário
-  dadosOpcoes = this.enviaDadosOpcoes.asObservable();
   enviaDados(dados: any) {
-    this.enviaDadosOpcoes.next(dados);
+    this.enviaDadosOpcoes.next(dados.optionsDrivers); // já manda só o array
   }
 }

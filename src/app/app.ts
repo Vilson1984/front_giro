df5efd1a6249765b95f17f8722efc111afa24143
Vilson C Monteiro <vcmorion@hotmail.com>
2026-06-29 07:29:35 -0300

    build history

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SolicitacaoViagem } from './features/solicitacao-viagem/pages/solicitacao-viagem/solicitacao-viagem';
 import { OpcoesViagem } from './features/opcoes-viagem/pages/opcoes-viagem/opcoes-viagem';
+import { Historico } from './features/historico-viagem/pages/historico/historico';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SolicitacaoViagem, OpcoesViagem],
+  imports: [RouterOutlet, SolicitacaoViagem, OpcoesViagem, Historico],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true

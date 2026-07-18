27fbc7bb446686576e1ec74a13a4a61c48967067
Vilson C Monteiro <vcmorion@hotmail.com>
2026-07-18 14:26:46 -0300

    "traz histórico"

--- src/app/features/historico-viagem/pages/historico/historico.ts
+++ src/app/features/historico-viagem/pages/historico/historico.ts
@@ -5,27 +5,29 @@ import { HistoricoServiceTs } from '../../service/historico.service.ts';
 
 @Component({
   selector: 'app-historico',
-  imports: [FormsModule, CommonModule, ],
+  imports: [FormsModule, CommonModule],
   templateUrl: './historico.html',
   styleUrl: './historico.scss',
 })
 export class Historico {
-  constructor(private historicoService:HistoricoServiceTs){}
+  constructor(private historicoService: HistoricoServiceTs) {}
 
-  idUsuario:string = ''
-  idMotorista:string = ''
-  
-  pesquisar(usuario:string, motorista:string){
-    this.idUsuario = usuario
-    this.idMotorista = motorista    
-    // console.log(
-    //   "this.idUsuario", this.idUsuario,
-    //   "this.idMotorista", this.idMotorista,
-    // )
-    console.log("RESULT: ",
-    this.historicoService.getHistorico(this.idUsuario, this.idMotorista)
-  )
+  idUsuario: string = '';
+  idMotorista: string = '';
+  historico: any[] = [];
 
+  pesquisar(usuario: string, motorista: string) {
+    this.idUsuario = usuario;
+    this.idMotorista = motorista;
+
+    this.historicoService.getHistorico(this.idUsuario, this.idMotorista).subscribe({
+      next: (dados) => {
+        this.historico = Array.isArray(dados) ? dados : [dados];
+        console.log('DADOS:', dados);
+        console.log('historico:', this.historico[0].rides);
+        this.historico = this.historico[0].rides; // Acessa a propriedade "rides" do primeiro elemento do array
+      },
+      error: (erro) => console.error('Erro ao buscar histórico', erro),
+    });
   }
-  
 }
--- src/app/features/historico-viagem/service/historico.service.ts.ts
+++ src/app/features/historico-viagem/service/historico.service.ts.ts
@@ -1,25 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
-import { subscribe } from 'diagnostics_channel';
-import { response } from 'express';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
+
+
 export class HistoricoServiceTs {
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  getHistorico(idUsuario:string, idMotorista:string){
-    // console.log(">>>>>>>>>>>>> getHistorico", idUsuario, idMotorista) 
-    const response =
-   this.http.get("http://localhost:3001/ride/"+idUsuario)
-    .subscribe(
-      {next: response => console.log(response)}
-    )
-
-    return response
+  getHistorico(idUsuario: string, idMotorista: string): Observable<any> {
+   console.log(this.http.get(`http://localhost:3001/ride/${idUsuario}?driver_id=${idMotorista}`));
+    return this.http.get(`http://localhost:3001/ride/${idUsuario}?driver_id=${idMotorista}`);
   }
 }
-
-// http://localhost:3001/ride/42
-// http://localhost:3001/ride/IdDoUsuario
